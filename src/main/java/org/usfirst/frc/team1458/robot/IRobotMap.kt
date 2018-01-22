304a2c937fd7d5ba1f0c4eb190444a5ac29a49d9
Anish <anish.singhani@gmail.com>
2018-01-21 21:05:40 -0800

    new chassis stuff

---
+++ src/main/java/org/usfirst/frc/team1458/robot/IRobotMap.kt
@@ -0,0 +1,10 @@
+package org.usfirst.frc.team1458.robot
+
+import org.usfirst.frc.team1458.lib.drive.TankDrive
+import org.usfirst.frc.team1458.lib.sensor.NavX
+
+
+interface IRobotMap {
+    val drivetrain : TankDrive
+    val navX : NavX
+}
--- src/main/java/org/usfirst/frc/team1458/robot/RobotMapFinalChassis.kt
+++ src/main/java/org/usfirst/frc/team1458/robot/RobotMapFinalChassis.kt
@@ -5,18 +5,18 @@ import org.usfirst.frc.team1458.lib.drive.TankDrive
 import org.usfirst.frc.team1458.lib.pid.PIDConstants
 import org.usfirst.frc.team1458.lib.sensor.NavX
 
-class RobotMapPracticeChassis: IRobotMap {
+class RobotMapFinalChassis: IRobotMap {
     override val drivetrain : TankDrive = TankDrive(
             leftMaster = SmartMotor.CANtalonSRX(12),
             rightMaster = SmartMotor.CANtalonSRX(15).inverted,
             leftMotors = arrayOf(SmartMotor.CANtalonSRX(10), SmartMotor.CANtalonSRX(11)),
             rightMotors = arrayOf(SmartMotor.CANtalonSRX(13), SmartMotor.CANtalonSRX(14)).map { it.inverted }.toTypedArray(),
             closedLoopControl = true,
-            wheelDiameter = 0.354,
-            closedLoopScaling = 18.0, // Max speed = 6 ft/sec
+            wheelDiameter = 0.5,
+            closedLoopScaling = 18.0, // TODO: determine
 
-            pidConstantsLowGearLeft = PIDConstants(0.15, kI = 0.001, kD = 0.01, kF = 1.0/6530.5),
-            pidConstantsLowGearRight = PIDConstants(0.15, kI = 0.001, kD = 0.01, kF = 1.0/6877.7)
+            pidConstantsLowGearLeft = PIDConstants(0.15, kI = 0.001, kD = 0.01, kF = 1.0/6530.5), // TODO: determine
+            pidConstantsLowGearRight = PIDConstants(0.15, kI = 0.001, kD = 0.01, kF = 1.0/6877.7) // TODO: determine
     )
 
     override val navX = NavX.Micro_I2C()
