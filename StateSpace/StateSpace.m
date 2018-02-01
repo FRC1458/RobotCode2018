af06a4f53482dcc078eaec92678cf80b66c7aa10
Anish <anish.singhani@gmail.com>
2018-01-31 23:27:18 -0800

    state space

--- StateSpace/StateSpace.m
+++ StateSpace/StateSpace.m
@@ -1,3 +1,5 @@
+clear all, close all, clc
+
 % Define Constants
 J = 8.0; % Moment of Intertia
 b = 0.3;  % Viscous Friction
@@ -18,6 +20,20 @@ C = [1   0   0];
 D = 0;
 
 % Create system
-G = ss(A, B, C, D);
-C = pid(0.1, 0, 0);
-closedLoop = feedback(G*C,1);
+sys = ss(A, B, C, D);
+
+%[u,t] = gensig('square',5,30,0.1);
+%lsim(sys,u,t)
+
+Kp = 1;
+for i = 1:3
+    con(:,:,i) = pid(Kp, 0.0, 5.0);
+    Kp = Kp + 10;
+end
+sys_cl = feedback(con*sys, 1);
+t = 0:0.001:10.0;
+set(gco, 'LineWidth', 8)  
+step(sys_cl(:,:,1), sys_cl(:,:,2), sys_cl(:,:,3), t)
+ylabel('Position, \theta (radians)')
+title('Response to a Step Reference with Different Values of K_p')
+legend('Kp = 1',  'Kp = 11',  'Kp = 21')
