a11e958a2a3ad2d1e125d6f1460e1f69920489d8
Jake Grellman <jake@example.com>
2018-02-03 09:56:03 -0800

    did the thing

--- src/main/java/org/usfirst/frc/team1458/lib/util/SoundPlayer.kt
+++ src/main/java/org/usfirst/frc/team1458/lib/util/SoundPlayer.kt
@@ -1,4 +1,26 @@
 package org.usfirst.frc.team1458.lib.util
 
+import java.net.HttpURLConnection
+import java.net.URL
+
 object SoundPlayer {
+    private val playHTTP = "http://raspberrypi.local:5000/play/"
+    private val stopHTTP = "http://raspberrypi.local:5000/stop"
+
+    fun play(sound: String){
+        val SoundHTTP = playHTTP + sound + ".wav"
+        val obj = URL(SoundHTTP)
+        with(obj.openConnection() as HttpURLConnection){
+            println("\nSending 'GET' request to URL : $url")
+            println("Response Code : $responseCode")
+        }
+    }
+
+    fun stop(){
+        val obj = URL(stopHTTP)
+        with(obj.openConnection() as HttpURLConnection){
+            println("\nSending 'GET' request to URL : $url")
+            println("Response Code : $responseCode")
+        }
+    }
 }
--- src/main/java/org/usfirst/frc/team1458/robot/Lift.kt
+++ src/main/java/org/usfirst/frc/team1458/robot/Lift.kt
@@ -1,4 +1,22 @@
 package org.usfirst.frc.team1458.robot
 
-class Lift {
+import org.usfirst.frc.team1458.lib.actuator.SmartMotor
+import org.usfirst.frc.team1458.lib.pid.PID
+import org.usfirst.frc.team1458.lib.pid.PIDConstants
+
+class Lift(tar:Double, liftMotor: SmartMotor) {
+    var tar:Double = tar
+    set(value)
+    {
+        field = value
+        pid.target=tar
+    }
+    val constants = PIDConstants(0.0,0.0,0.0,0.0, { 0.0 })
+    val pid: PID = PID(constants,tar)
+
+
+
+
+
+
 }
