f00df9b6102a793c91c47253615b51c275e0f55b
1458 <redties1458@gmail.com>
2018-02-11 00:23:55 +0000

    Add new file

---
+++ test.php.kt
@@ -0,0 +1,14 @@
+package org.usfirst.frc.team1458.robot
+
+class Robot : BaseRobot() {
+    val oi = OI()
+    val robot = RobotMapPracticeChassis()
+
+    override fun robotSetup() {
+        Compressor().start()
+    }
+
+    override fun teleopPeriodic() {
+        robot.drivetrain.cheesyDrive(oi.throttleAxis.value, oi.steerAxis.value, oi.quickturnButton.triggered)
+    }
+}
