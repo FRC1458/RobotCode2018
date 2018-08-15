04c0b2cd5501581ff7563d0275ec12694d33e62c
Nick <ndp1234567890@gmail.com>
2018-08-15 06:45:56 -0700

    Started doing NLRT stuff

--- src/main/java/org/usfirst/frc/team1458/lib/pathfinding/NLRTSplineFollower.kt
+++ src/main/java/org/usfirst/frc/team1458/lib/pathfinding/NLRTSplineFollower.kt
@@ -11,10 +11,8 @@ import org.usfirst.frc.team1458.lib.util.flow.systemTimeMillis
 import java.io.File
 
 
-class SplineFollower(val left: Trajectory,
-                     val right: Trajectory,
+class NLRTSplineFollower(val mainTrajectory: Trajectory,
                      val drivetrain: TankDrive,
-                     dt: Double? = null,
                      val gyro: AngleSensor? = null,
                      val gyro_kP: Double? = null,
                      name: String,
@@ -23,41 +21,49 @@ class SplineFollower(val left: Trajectory,
                      val everyIterationFunc: () -> Unit = { }
                     ) : BaseAutoMode() {
 
-    constructor(leftCSV: String, rightCSV: String, drivetrain: TankDrive,
-                dt: Double? = null, gyro: AngleSensor? = null, gyro_kP: Double? = null,
+    constructor(mainTrajectoryCSV: String, drivetrain: TankDrive, gyro: AngleSensor? = null, gyro_kP: Double? = null,
                 name: String = "Spline" +
-                        leftCSV.split("/").last().replace("leftTrajectory", ""),
+                        mainTrajectoryCSV.split("/").last().replace("leftTrajectory", ""),
                 stopFunc: () -> Boolean = { false },
                 reversed: Boolean = false,
                 everyIterationFunc: () -> Unit = { }) :
-            this(Pathfinder.readFromCSV(File(leftCSV)), Pathfinder.readFromCSV(File(rightCSV)),
-                    drivetrain, dt, gyro, gyro_kP, name, stopFunc, reversed, everyIterationFunc)
+            this(Pathfinder.readFromCSV(File(mainTrajectoryCSV)), drivetrain, gyro,
+                    gyro_kP, name, stopFunc, reversed, everyIterationFunc)
 
-    val dt: Double = dt ?: (left[0].dt * 1000.0) // Delta Time: Time between pathfinder points
+    val pathfinderDeltaTime: Double = (mainTrajectory[0].dt * 1000.0) // Delta Time: Time between pathfinder points
     override val name = name
 
     override fun auto() {
-        val startTime = systemTimeMillis
-        fun getIndex() = Math.floor((systemTimeMillis - startTime) / dt).toInt()
+        val startTime = systemTimeMillis // Sets time start-point
+        fun getIndex() = Math.floor((systemTimeMillis - startTime) / pathfinderDeltaTime).toInt()
 
-
-        var index : Int = getIndex()
-        gyro?.zero()
+        var trajectoryIndex : Int = getIndex() // Fetch how far down the path the robot is
+        gyro?.zero() // Zero the gyroscope
 
         // While the path is still being followed AND stop function is false...
-        while((index) < (left.length()) && !stopFunc()) {
+        while((trajectoryIndex) < (mainTrajectory.length()) && !stopFunc()) {
             //AutoDataLogger.currentIterationTimestamp = systemTimeMillis
 
-            var leftVel = left[index].velocity
-            var rightVel = right[index].velocity
+            var desiredVelocity = mainTrajectory[trajectoryIndex].velocity
 
             if(gyro != null && gyro_kP != null) {
-                val angleError = Pathfinder.boundHalfDegrees(- Pathfinder.boundHalfDegrees(gyro.heading) - Pathfinder.boundHalfDegrees(Pathfinder.r2d(left[index].heading)))
+                var thetaError = Pathfinder.boundHalfDegrees(- Pathfinder.boundHalfDegrees(gyro.heading) - Pathfinder.boundHalfDegrees(Pathfinder.r2d(mainTrajectory[index].heading)))
                 //println(angleError)
+
+                val xError = mainTrajectory[trajectoryIndex].x - robotX
+                val yError = mainTrajectory[trajectoryIndex].y - robotY
+
+                if (kotlin.math.abs(thetaError) < 0.0001) {
+                    thetaError = 0.0001
+                }
+
+                val vFF = mainTrajectory[trajectoryIndex].velocity
+                val wFF = mainTrajectory[trajectoryIndex].angular_velocity
+                val theta = mainTrajectory[trajectoryIndex].heading
 
                 val turnAdjustment = gyro_kP * angleError
 
-                leftVel -= turnAdjustment
+                mainVel -= turnAdjustment
                 rightVel += turnAdjustment
             }
             if(reversed){
@@ -66,9 +72,10 @@ class SplineFollower(val left: Trajectory,
             }
 
             if(true) {
-                drivetrain.setDriveVelocity(leftVel, rightVel)
+                drivetrain.setDriveVelocity(mainVel, mainVel) // Needs left and right velocity
             } else {
-                drivetrain.setOpenLoopDrive(leftVel / (drivetrain.closedLoopScaling ?: 1000000.0), rightVel / (drivetrain.closedLoopScaling ?: 1000000.0))
+                drivetrain.setOpenLoopDrive(leftVel / (drivetrain.closedLoopScaling ?: 1000000.0),
+                        rightVel / (drivetrain.closedLoopScaling ?: 1000000.0))
             }
             everyIterationFunc()
 
@@ -77,8 +84,7 @@ class SplineFollower(val left: Trajectory,
 
             //println("${leftTrajectory[index].velocity},${leftVel},${rightTrajectory[index].velocity},${rightVel},${rightTrajectory[index].heading},${gyro?.angle ?: 0.0},${drivetrain.leftMaster.connectedEncoder.rate * 1.11 / 360.0},${drivetrain.rightMaster.connectedEncoder.rate * 1.11 / 360.0},${drivetrain.leftMaster._talonInstance!!.getClosedLoopError(0)},${drivetrain.rightMaster._talonInstance!!.getClosedLoopError(0)}")
 
-            SmartDashboard.putNumber("Left Velocity", left[index].velocity)
-            SmartDashboard.putNumber("Right Velocity", right[index].velocity)
+            SmartDashboard.putNumber("Main Velocity", mainTrajectory[trajectoryIndex].velocity)
 
             SmartDashboard.putNumber("Real Left Velocity", drivetrain.leftMaster.connectedEncoder.rate * 1.11 / 360.0)
             SmartDashboard.putNumber("Real Right Velocity", drivetrain.rightMaster.connectedEncoder.rate * 1.11 / 360.0)
@@ -101,7 +107,7 @@ class SplineFollower(val left: Trajectory,
             System.out.println("")*/
 
             delay(10)
-            index = getIndex()
+            trajectoryIndex = getIndex()
         }
         drivetrain.setRawDrive(0.0,0.0)
 
